d1278bbc66b1506c3c4caf447b7afd4c410bd97e
James Blacklock <jamesblacklock@protonmail.com>
2022-02-21 12:16:49 -0500

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,16 @@
 const express = require('express');
- const router = express
+const router = express.Router();
 
-// register
-// login
-// logout
+router.post('register', (req, res, next) => {
+    res.json('register')
+});
+
+router.post('login', (req, res, next) => {
+    res.json('login')
+});
+
+router.get('logout', (req, res, next) => {
+    res.json('logout')
+});
+
+module.exports = router;
